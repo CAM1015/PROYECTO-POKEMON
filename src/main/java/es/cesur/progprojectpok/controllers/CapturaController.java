14d23036d1c35d2d7d6133a2cec5d25149bdd94c
claudiaalonso <alonsoclaudia526@gmail.com>
2024-05-16 23:55:43 +0200

    La captura funciona perfectamente: genera un pokemon aleatorio y añade a la base de datos el pokemon capturado. Falta añadir la función que permita darle un mote.

--- src/main/java/es/cesur/progprojectpok/controllers/CapturaController.java
+++ src/main/java/es/cesur/progprojectpok/controllers/CapturaController.java
@@ -1,6 +1,7 @@
 package es.cesur.progprojectpok.controllers;
 
 import es.cesur.progprojectpok.SplashApplication;
+import es.cesur.progprojectpok.database.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +9,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CapturaController implements Initializable {
@@ -33,8 +42,19 @@ public class CapturaController implements Initializable {
     @FXML
     private Label lblNombrePokemonSalvaje;
 
+    @FXML
+    private ImageView imagenPokemon;
+
+    private int numPokedexGenerado;
+
+    String nombrePokemonGenerado = null;
 
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lblCapturaResultado.setVisible(false);
+    }
     @FXML
     protected void onVolverBtnClick() {
         try {
@@ -56,21 +76,132 @@ public class CapturaController implements Initializable {
         }
     }
 
-
-    public void onCapturarClick(ActionEvent actionEvent) {
-    }
-
-    public void onMenuPrincipalClick(ActionEvent actionEvent) {
-    }
-
+    @FXML
     public void onGenerarPokemonAleatorioClick(ActionEvent actionEvent) {
-    }
+            Random random = new Random();
+            int randomNumber = random.nextInt(26) + 1;
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+            Connection connection = DBConnection.getConnection();
+            String sql = "SELECT * FROM pokedex WHERE NUM_POKEDEX = " + randomNumber;
 
-    }
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    int NUM_POKEDEX = resultSet.getInt("NUM_POKEDEX");
+                    String nombrePokemonGenerado = resultSet.getString("NOM_POKEMON");
+                    String TIPO1 = resultSet.getString("TIPO1");
+                    String TIPO2 = resultSet.getString("TIPO2");
+                    String ImagenUrlPokemonGenerado = resultSet.getString("IMAGEN");
+                    int NIVEL_EVOLUCION = resultSet.getInt("NIVEL_EVOLUCION");
+                    int NUM_POKEDEX_EVO = resultSet.getInt("NUM_POKEDEX_EVO");
+
+                    System.out.println(NUM_POKEDEX + " " + nombrePokemonGenerado + " " + TIPO1 + " " + TIPO2 + " " + ImagenUrlPokemonGenerado + " " + NIVEL_EVOLUCION + " " + NUM_POKEDEX_EVO);
+
+                    //Carga de imagen
+                    Image imagenPokemonGenerado = new Image("file:" + ImagenUrlPokemonGenerado);
+
+                    imagenPokemon.setImage(imagenPokemonGenerado);
+                    imagenPokemon.setOpacity(1);
+
+                    lblNombrePokemonSalvaje.setText(nombrePokemonGenerado);
+                    lblNombrePokemonSalvaje.setOpacity(1);
+                    numPokedexGenerado = NUM_POKEDEX;
+                }
+            } catch (NullPointerException | SQLException e) {
+                System.out.println("CapturaController NullPointerException");
+            }
+        }
+
+
+
+        public void onCapturarClick(ActionEvent actionEvent) {
+
+            Random randomProbabilidadCaptura = new Random();
+            int probabilidadCaptura = randomProbabilidadCaptura.nextInt(100) + 1;
+
+            if (probabilidadCaptura < 67) {
+                try {
+                    Random random = new Random();
+
+                    int numPokedex = numPokedexGenerado;
+                    int idEntrenador = SesionController.getInstance().getEntrenadorId();
+                    String mote = "Mote por definir.";
+                    int caja = 2;
+                    int ataque = random.nextInt(10) + 1;
+                    int ataqueEspecial = random.nextInt(10) + 1;
+                    int defensa = random.nextInt(10) + 1;
+                    int defensaEspecial = random.nextInt(10) + 1;
+                    int velocidad = random.nextInt(10) + 1;
+                    int fertilidad = 5;
+                    char sexo = (random.nextBoolean()) ? 'M' : 'F';
+                    String estado = "";
+                    int experiencia = random.nextInt(10) + 1;
+                    int vitalidad = random.nextInt(10) + 1;
+                    int idObjeto = 0;
+                    int nivel = random.nextInt(10) + 1;
+
+
+
+
+                    Connection connection = DBConnection.getConnection();
+                    String insertSql = "INSERT INTO pokemon (NUM_POKEDEX, ID_ENTRENADOR, MOTE, CAJA, ATAQUE, AT_ESPECIAL, DEFENSA, DEF_ESPECIAL, VELOCIDAD, FERTILIDAD, SEXO, ESTADO, EXPERIENCIA, VITALIDAD, ID_OBJETO, NIVEL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//insert en pokedex: NOM_POKEMON
+
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(insertSql)) {
+                        preparedStatement.setInt(1, numPokedex);
+                        preparedStatement.setInt(2, idEntrenador);
+                        preparedStatement.setString(3, mote);
+                        preparedStatement.setInt(4, caja);
+                        preparedStatement.setInt(5, ataque);
+                        preparedStatement.setInt(6, ataqueEspecial);
+                        preparedStatement.setInt(7, defensa);
+                        preparedStatement.setInt(8, defensaEspecial);
+                        preparedStatement.setInt(9, velocidad);
+                        preparedStatement.setInt(10, fertilidad);
+                        preparedStatement.setString(11, String.valueOf(sexo));
+                        preparedStatement.setString(12, estado);
+                        preparedStatement.setInt(13, experiencia);
+                        preparedStatement.setInt(14, vitalidad);
+                        preparedStatement.setInt(15, idObjeto);
+                        preparedStatement.setInt(16, nivel);
+
+
+
+
+
+
+                        System.out.println(insertSql);
+
+
+                        int rowsAffected = preparedStatement.executeUpdate();
+
+                        if (rowsAffected > 0) {
+                            System.out.println("Pokemon capturado y guardado en la base de datos.");
+                        } else {
+                            System.out.println("Error al guardar el Pokémon en la base de datos.");
+                        }
+                    }
+                    System.out.println("¡Pokemon capturado!");
+                    lblCapturaResultado.setText("¡Pokemon capturado!");
+                    lblCapturaResultado.setVisible(true);
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("El pokemon ha escapado :(");
+                lblCapturaResultado.setText("El pokemon ha escapado :(");
+                lblCapturaResultado.setVisible(true);
+
+            }
+                }
+
+
+
 
 
 }
